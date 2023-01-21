aba7f268ff54caa4e3e201a94df9f935dca56b13
dongkeonkim <dongkeonkim0810@gmail.com>
2023-01-21 20:28:02 +0900

    2023. 01. 21 브랜치 생성

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,4 +1,4 @@
-export { default as BoardList } from "./BoardList";
-export { default as BoardNew } from "./BoardNew";
-export { default as BoardDetail } from "./BoardDetail";
-export { default as BoardUpdate } from "./BoardUpdate";
+export {default as BoardList} from './BoardList';
+export {default as BoardNew} from './BoardNew';
+export {default as BoardDetail} from './BoardDetail';
+export {default as BoardUpdate} from './BoardUpdate';
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-import "../../../src/style.css";
+import '../../../src/style.css';
 
 const Nav = () => {
   return (
-    <div className="relative bg-white dark:bg-gray-800">
-      <div className="flex flex-col sm:flex-row sm:justify-around">
-        <div className="h-screen w-72">
-          <nav className="mt-10 px-6 ">
+    <div className='relative bg-white dark:bg-gray-800'>
+      <div className='flex flex-col sm:flex-row sm:justify-around'>
+        <div className='h-screen w-72'>
+          <nav className='mt-10 px-6 '>
             <Link
-              to="/"
-              className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-1 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 text-gray-600 dark:text-gray-400 rounded-lg"
-            >
-              <span className="mx-4 text-lg font-normal">게시판</span>
+              to='/'
+              className='hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-1 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 text-gray-600 dark:text-gray-400 rounded-lg'>
+              <span className='mx-4 text-lg font-normal'>게시판</span>
             </Link>
             <Link
-              to="/join"
-              className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-1 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 text-gray-600 dark:text-gray-400 rounded-lg"
-            >
-              <span className="mx-4 text-lg font-normal">로그인</span>
+              to='/join'
+              className='hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-1 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200 text-gray-600 dark:text-gray-400 rounded-lg'>
+              <span className='mx-4 text-lg font-normal'>로그인</span>
             </Link>
           </nav>
         </div>
